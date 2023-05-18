cddec76647571c62343e0af594fcde349f6ea69d
leninman <leninmanrique@gmail.com>
2023-05-17 21:11:23 -0400

    Ajustes en el target

--- target/generated-sources/annotations/com/tecnodestreza/siga/models/Materia_.java
+++ target/generated-sources/annotations/com/tecnodestreza/siga/models/Materia_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-14T19:47:14")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-17T21:10:08")
 @StaticMetamodel(Materia.class)
 public class Materia_ { 
 
