40e55b8d16c33b54fe292cb024b702a82948239f
leninman <leninmanrique@gmail.com>
2023-09-04 13:07:53 -0400

    Servicio obtener profesiones

--- target/generated-sources/annotations/com/tecnodestreza/siga/models/NotaParcial_.java
+++ target/generated-sources/annotations/com/tecnodestreza/siga/models/NotaParcial_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-04T12:59:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-04T13:06:07")
 @StaticMetamodel(NotaParcial.class)
 public class NotaParcial_ { 
 
