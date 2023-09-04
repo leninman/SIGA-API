f097a735e6be926d2c377031a93e70fac70e78c0
leninman <leninmanrique@gmail.com>
2023-09-04 13:42:58 -0400

    Obtencion de profesiones

--- target/generated-sources/annotations/com/tecnodestreza/siga/models/Materia_.java
+++ target/generated-sources/annotations/com/tecnodestreza/siga/models/Materia_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-04T13:06:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-04T13:40:47")
 @StaticMetamodel(Materia.class)
 public class Materia_ { 
 
