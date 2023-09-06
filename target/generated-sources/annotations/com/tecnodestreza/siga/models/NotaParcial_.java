10f871d8ffd01d8e27442c1af7a4c4977ada6ab5
leninman <leninmanrique@gmail.com>
2023-09-06 16:46:07 -0400

    Implementacion de profiles

--- target/generated-sources/annotations/com/tecnodestreza/siga/models/NotaParcial_.java
+++ target/generated-sources/annotations/com/tecnodestreza/siga/models/NotaParcial_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-04T13:40:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-06T16:44:31")
 @StaticMetamodel(NotaParcial.class)
 public class NotaParcial_ { 
 
--- target/generated-sources/annotations/com/tecnodestreza/siga/models/Representante_.java
+++ target/generated-sources/annotations/com/tecnodestreza/siga/models/Representante_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-04T13:40:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-06T16:44:31")
 @StaticMetamodel(Representante.class)
 public class Representante_ { 
 
